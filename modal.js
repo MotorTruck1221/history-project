eea44b8f8cbf5fb8d3f0ee7b3635ffeb31d3a7a2
MotorTruck1221 <73721704+MotorTruck1221@users.noreply.github.com>
2021-11-17 17:55:54 -0700

    Added Test Modal And Bootstrap

--- modal.js
+++ modal.js
@@ -1,9 +1,10 @@
-// Get the modal
-var modal = document.getElementById('id01');
+$(document).ready(function () {
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
+  $("#btnShow").click(function () {
+
+      $('#SampleModal').modal('show');
+
+  });
+
+});
+
